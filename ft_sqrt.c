6ae2b6b8576a1e50eb175c7726c83a5f4e8b811e
stasbovino <stasbovino@mail.ru>
2019-03-12 18:51:33 +0300

    added sqrt

--- ft_sqrt.c
+++ ft_sqrt.c
@@ -2,4 +2,21 @@
 
 size_t	ft_sqrt(size_t nb, int opt)
 {
+	size_t	i;
+
+	i = 0;
+	while (i < nb / 2)
+	{
+		if (i * i < nb && (i + 1) * (i + 1) > nb)
+		{
+			if (opt = -1)
+				return (i);
+			else
+				return (i + 1);
+		}
+		else
+			if (i * i == nb)
+				return (i);
+		i++;
+	}
 }
