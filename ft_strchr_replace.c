58fe34cc2433e7c554f9a03f542d0aefa568e22a
stasbovino <stasbovino@mail.ru>
2019-04-06 21:42:54 +0300

    upd

--- ft_strchr_replace.c
+++ ft_strchr_replace.c
@@ -1,13 +1,13 @@
 #include "libft.h"
 
-void	ft_strchr_replace(char *s, int old, int new)
+void	ft_strchr_replace(char **s, int old, int new)
 {
 	size_t	i;
 
-	if (!s || !*s)
+	if (!s || !*s || !**s)
 		return ;
 	i = -1;
-	while (s[++i])
-		if (s[i] == old)
-			s[i] = new;
+	while ((*s)[++i])
+		if ((*s)[i] == old)
+			(*s)[i] = new;
 }
