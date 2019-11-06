8ea37b5290f0a8c1b09cd6f539832135c08c8459
stasbovino <stasbovino@mail.ru>
2019-11-06 14:20:29 +0300

    fixed

--- printfsrc/prepare_opt.c
+++ printfsrc/prepare_opt.c
@@ -6,7 +6,7 @@
 /*   By: gwyman-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/11 19:27:11 by gwyman-m          #+#    #+#             */
-/*   Updated: 2019/11/05 18:56:15 by gwyman-m         ###   ########.fr       */
+/*   Updated: 2019/11/06 14:20:11 by gwyman-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,6 @@ static t_options	*create_opt_strct(void)
 	return (new);
 }
 
-void				wdcd(int *dst, va_list *args, size_t *inc, int opt)
-{
-	*dst = va_arg(*args, int);
-	*inc += 1;
-	if (opt == 1)
-		*inc += 1;
-}
-
 int					prepare_opt(char *format, size_t *begin, va_list *args)
 {
 	size_t		i;
@@ -46,15 +38,8 @@ int					prepare_opt(char *format, size_t *begin, va_list *args)
 	opt = create_opt_strct();
 	while (flags(opt, format[i]))
 		i++;
-<<<<<<< HEAD
 	define_width(format, &(opt->width), &i, args);
 	define_prec(format, &(opt->prec), &i);
-=======
-	format[i] == '*' ? wdcd(&(opt->width), args, &i, 0) :
-		define_width(format, &(opt->width), &i);
-	(format[i] == '.' && format[i + 1] == '*') ?
-		wdcd(&(opt->prec), args, &i, 1) : define_prec(format, &(opt->prec), &i);
->>>>>>> b580b53fc9cb1cdf1f9b531b0750d4a1eca2a1f2
 	parse_len_mod(opt, format + i);
 	if (opt->len_mod)
 		i += ft_strlen(opt->len_mod);
